98eca9b8bf4124d8f99e557554289be12c891816
Marllysonnn <marllyson.alves797@gmail.com>
2023-11-26 04:23:08 -0300

    Tela de homem pronta

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 const input = document.querySelector('input');
 const pesquisa = document.querySelector('.pesquisa');
-let isInputOpen = false;
+const lupa = document.querySelector('.lupa')
+let inputAberto = false;
 
 function abrirInput() {
   input.style.width = '22em';
   input.style.transition = '0.5s';
-  isInputOpen = true;
+  inputAberto = true;
 }
 
 function fecharInput() {
-  if (isInputOpen) {
+  if (inputAberto) {
     input.style.width = '0em';
     input.style.transition = '0.5s';
-    isInputOpen = false;
+    inputAberto = false;
   }
 }
 
 function handleClickOutside(event) {
   // Verifica se o clique ocorreu fora do input e fecha-o se estiver aberto
-  if (isInputOpen && !pesquisa.contains(event.target) && !input.contains(event.target)) {
+  if (inputAberto && !lupa.contains(event.target) && !input.contains(event.target)) {
     fecharInput();
   }
 }
 
 window.addEventListener('click', handleClickOutside);
-pesquisa.addEventListener('click', abrirInput);
+lupa.addEventListener('click', abrirInput);
