195f196d40a3c5122145011b8c7b93f6f201eff3
Marllyson Alves <115300307+Marllysonnn@users.noreply.github.com>
2023-12-03 02:56:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const input = document.querySelector('.input');
 const lupa = document.querySelector('.lupa');
-const scroll = document.querySelconst input = document.querySelector('.input');
-const lupa = document.querySelector('.lupa');
 const scroll = document.querySelector('.carrossel');
 const container = document.querySelector('.container');
 const scroll2 = document.querySelector('.carrossel2');
@@ -131,76 +129,3 @@ racoes.addEventListener('click', rollingRacao)
 brinquedos.addEventListener('click', rollingBrinquedo);
 casas.addEventListener('click', rollingCasas)
 
-ector('.carrossel');
-const container = document.querySelector('.container');
-const left = document.querySelector('#left');
-const right = document.querySelector('#right');
-
-let inputAberto = false;
-let marginLeftValue = 0;
-
-function abrirInput() {
-  input.style.width = '22em';
-  input.style.transition = '0.5s';
-  inputAberto = true;
-}
-
-function fecharInput() {
-  if (inputAberto) {
-    input.style.width = '0em';
-    input.style.transition = '0.5s';
-    inputAberto = false;
-  }
-}
-
-function handleClickOutside(event) {
-  if (inputAberto && !lupa.contains(event.target) && !input.contains(event.target)) {
-    fecharInput();
-  }
-}
-
-function arrastar(index) {
-  const radioButton = document.getElementById('opcao' + (index + 1));
-  radioButton.checked = true;
-  if (radioButton.checked) {
-    marginLeftValue = -51 * index;
-    scroll.style.marginLeft = marginLeftValue + 'em';
-    scroll.style.transition = '2s';
-    const newIndex = Math.abs(marginLeftValue / 51);
-    updateRadioState(newIndex);
-  }
-}
-
-function leftDirec() {
-  if (marginLeftValue > -102) {
-    marginLeftValue -= 51;
-    scroll.style.marginLeft = marginLeftValue + 'em';
-    scroll.style.transition = '2s';
-    const index = Math.abs(marginLeftValue / 51);
-    updateRadioState(index);
-  }
-}
-
-function rightDirec() {
-  if (marginLeftValue < 0) {
-    marginLeftValue += 51;
-    scroll.style.marginLeft = marginLeftValue + 'em';
-    scroll.style.transition = '2s';
-    const index = Math.abs(marginLeftValue / 51);
-    updateRadioState(index);
-    
-  }
-}
-
-function updateRadioState(index) {
-  const radioButtons = document.getElementsByName('opcao');
-  radioButtons.forEach((radioButton, i) => {
-    radioButton.checked = i === index;
-  });
-}
-
-window.addEventListener('click', handleClickOutside);
-left.addEventListener('click', rightDirec);
-right.addEventListener('click', leftDirec);
-container.addEventListener('click', arrastar);
-lupa.addEventListener('click', abrirInput);
