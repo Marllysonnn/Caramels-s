018d17d29118d82070edd31b987cd198330fba2b
Marllysonnn <marllyson.alves797@gmail.com>
2023-12-03 14:34:05 -0300

    header ajustado

--- script.js
+++ script.js
@@ -2,22 +2,30 @@ const input = document.querySelector('.input');
 const lupa = document.querySelector('.lupa');
 const scroll = document.querySelector('.carrossel');
 const container = document.querySelector('.container');
+const scroll2 = document.querySelector('.carrossel2');
+const container2 = document.querySelector('.container2');
 const left = document.querySelector('#left');
 const right = document.querySelector('#right');
+const left2 = document.querySelector('#left2');
+const right2 = document.querySelector('#right2');
+const rolling = document.querySelector('.container_grid')
+const racoes = document.querySelector(".racao");
+const brinquedos = document.querySelector(".brinquedos");
+const casas = document.querySelector(".casas");
 
 let inputAberto = false;
 let marginLeftValue = 0;
+let marginLeftValue2 = 0;
 
 function abrirInput() {
-  input.style.width = '22em';
+  input.style.width = '18em';
   input.style.transition = '0.5s';
   inputAberto = true;
 }
 
 function fecharInput() {
   if (inputAberto) {
-    input.style.width = '0em';
-    input.style.transition = '0.5s';
+    input.style.width = '15em';
     inputAberto = false;
   }
 }
@@ -49,6 +57,13 @@ function leftDirec() {
     updateRadioState(index);
   }
 }
+function leftDirec2() {
+  if (marginLeftValue2 > -102) {
+    marginLeftValue2 -= 51;
+    scroll2.style.marginLeft = marginLeftValue2 + 'em';
+    scroll2.style.transition = '2s';
+  }
+}
 
 function rightDirec() {
   if (marginLeftValue < 0) {
@@ -60,6 +75,13 @@ function rightDirec() {
     
   }
 }
+function rightDirec2() {
+  if (marginLeftValue2 < 0) {
+    marginLeftValue2 += 51;
+    scroll2.style.marginLeft = marginLeftValue2 + 'em';
+    scroll2.style.transition = '2s';
+  }
+}
 
 function updateRadioState(index) {
   const radioButtons = document.getElementsByName('opcao');
@@ -68,8 +90,42 @@ function updateRadioState(index) {
   });
 }
 
+function rollingRacao() {
+  rolling.style.marginLeft = "200%";
+  rolling.style.transition = '2s';
+}
+function rollingBrinquedo() {
+    rolling.style.marginLeft = '0%';
+    rolling.style.transition = '2s';
+}
+function rollingCasas() {
+  rolling.style.marginLeft = "-200%";
+  rolling.style.transition = '2s';
+}
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 80) {
+    header.style.background = "#EDAB46";
+    header.style.transition = "0.5s"
+    header.style.borderBottom = "solid #966823";
+  } else {
+    header.style.background = "";
+    header.style.borderBottom = "none";
+  }
+});
+
+
 window.addEventListener('click', handleClickOutside);
 left.addEventListener('click', rightDirec);
 right.addEventListener('click', leftDirec);
+left2.addEventListener('click', rightDirec2);
+right2.addEventListener('click', leftDirec2);
 container.addEventListener('click', arrastar);
-lupa.addEventListener('click', abrirInput);
+input.addEventListener('click', abrirInput);
+racoes.addEventListener('click', rollingRacao)
+brinquedos.addEventListener('click', rollingBrinquedo);
+casas.addEventListener('click', rollingCasas)
+
