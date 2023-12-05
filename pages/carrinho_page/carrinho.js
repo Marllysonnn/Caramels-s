27c7b9d27e1593a5678449dad42efefd9e336167
Marllyson Alves <115300307+Marllysonnn@users.noreply.github.com>
2023-12-05 09:11:10 -0300

    Create carrinho.js

---
+++ pages/carrinho_page/carrinho.js
@@ -0,0 +1,8 @@
+const excluir = document.querySelector('.excluir')
+const deletar = document.querySelector('.container')
+
+function deletarProduto () {
+  deletar.style.display = "none";
+}
+
+excluir.addEventListener('click', deletarPrpduto)
